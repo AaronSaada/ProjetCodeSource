2fb00318be986b39b6ce4cb149acdbc7fc6c2534
AaronSaada <127952327+AaronSaada@users.noreply.github.com>
2024-12-10 11:22:03 +0100

    modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,8 @@
 console.log('Hello World!')
 
-
+// Expliciter le type de la variable
+const myVar:string = "Hello World!"
+const myNumber:number = 2
+const myBoolean:boolean = true
+const myArray:string[] = ["Hello", "World", "!"]
 
