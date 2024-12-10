f9914dcaebffc6d39a718ae047556ce7bb300fd5
AaronSaada <127952327+AaronSaada@users.noreply.github.com>
2024-12-10 14:54:57 +0100

    Changement ESlint

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import eslint from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,5 +8,4 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslint,
 ]
