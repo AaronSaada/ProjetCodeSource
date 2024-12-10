717a4ac393b5c3b2fb9871085b4a0ad0ed714189
AaronSaada <127952327+AaronSaada@users.noreply.github.com>
2024-12-10 16:47:04 +0100

    docker

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,20 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World !')
 })
 
+app.post('/login', (req, res) => {
+  if (req.body.password === 'test' && req.body.login === 'test') {
+    res.send('Connected!')
+  }else{
+    res.sendStatus(401)
+  }
+})
+
 app.listen(3000, () => {
-  
+  console.log("Listening on port 3000")
 })
