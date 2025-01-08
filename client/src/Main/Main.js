a195b8c5ffd667d44af7be13305d320dd8140048
AaronSaada <127952327+AaronSaada@users.noreply.github.com>
2025-01-08 17:26:31 +0100

    CSS

---
+++ client/src/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Main() {
+
+    return (
+
+        <main>
+            <button>
+                Changer la couleur
+            </button>
+        </main>
+        
+    )
+}
+
+export default Main
